555a96df3553c5d9abe1355f52f765ddf2a6474e
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-02-19 18:40:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+// Your code  goes here
+
+// document.addEventListener("DOMContentLoaded", domLoaded, false);
+//
+// function domLoaded(event) {
+//   alert("The DOM has loaded");
+// }
+
+function updateDOM() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
+
+function domContentLoaded() {
+  document.addEventListener("DOMContentLoaded", function(event) {
+    updateDOM()
+  });
+}
